857fccef2a4a13e3b41e346cdd8961726fcdc9f0
Wilfredo Sulbaran <wilfredosulbaran94@gmail.com>
2024-01-08 19:11:46 -0500

    feature: created api email-parse and add  test

--- src/modules/email-parse/email-parse.controller.ts
+++ src/modules/email-parse/email-parse.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
 import { EmailParseService } from './email-parse.service';
+import { Response } from 'express';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('email-parse')
+@ApiTags('Email Parse')
 export class EmailParseController {
   constructor(private readonly emailParseService: EmailParseService) {}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.emailParseService.findOne(+id);
+  @Get(':path')
+  findOne(@Param('path') path: string, @Res() res: Response) {
+    this.emailParseService
+      .getEmailJson(path)
+      .then((result) => {
+        if (result.isLink) {
+          res.redirect(result.link);
+        } else {
+          res.status(HttpStatus.OK).json(result.jsonData);
+        }
+      })
+      .catch((error) => {
+        res.status(HttpStatus.NOT_FOUND).send(error);
+      });
   }
 }
--- src/modules/email-parse/email-parse.service.ts
+++ src/modules/email-parse/email-parse.service.ts
@@ -1,8 +1,53 @@
 import { Injectable } from '@nestjs/common';
-
+import * as fs from 'fs';
+import * as path from 'path';
+import * as parser from 'mailparser';
 @Injectable()
 export class EmailParseService {
-  findOne(id: number) {
-    return `This action returns a #${id} emailParse`;
+  async getEmailJson(filename: string) {
+    try {
+      const pathEmail = this.getPathEmail(filename);
+      const parsedEmail = await this.parseEmail(pathEmail);
+      return this.readEmail(parsedEmail);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  getPathEmail(filename: string): string {
+    const srcFolderPath = __dirname;
+    const rootFolderPath = path.resolve(srcFolderPath, '../../../');
+    const filePath = path.join(rootFolderPath, 'emails', filename);
+    if (fs.existsSync(filePath)) {
+      return filePath;
+    } else {
+      throw 'Email file not found';
+    }
+  }
+
+  async parseEmail(filePath: string): Promise<any> {
+    const content = fs.readFileSync(filePath, 'utf-8');
+    const parsedEmail = await parser.simpleParser(content);
+    return parsedEmail;
+  }
+
+  readEmail(emailData) {
+    const jsonAttachment = emailData.attachments.find(
+      (attachment) => attachment.contentType === 'application/json',
+    );
+
+    if (jsonAttachment) {
+      const jsonData = JSON.parse(jsonAttachment.content.toString());
+      return { jsonData };
+    } else {
+      const bodyText = emailData.text || '';
+      const linkMatch = bodyText.match(/(?:http|https):\/\/\S+\.json/);
+      if (linkMatch) {
+        const link = linkMatch[0];
+        return { isLink: true, link };
+      } else {
+        throw 'JSON not found in the email';
+      }
+    }
   }
 }
